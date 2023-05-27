98403bd1ce34ecfcfc1a67c9e9335d2c66f55751
Rujal shrestha <rujalshrestha66@gmail.com>
2023-05-27 17:55:09 +0545

    add1

--- src/pages/SearchPage.js
+++ src/pages/SearchPage.js
@@ -1,9 +1,11 @@
 import React from 'react'
-import { useParams } from 'react-router'
+import { useParams,useNavigate } from 'react-router'
 import { useGetSearchMovieQuery } from '../features/movie_api';
-import { Image, Shimmer } from 'react-shimmer';
+// import { Image, Shimmer } from 'react-shimmer';
 
 const SearchPage = () => {
+
+  const nav = useNavigate()
   const { searchText } = useParams();
 
   const {  data } = useGetSearchMovieQuery(searchText);
@@ -11,27 +13,19 @@ const SearchPage = () => {
  
 
   return (
-
-    <div className='grid grid-cols-2 sm:grid-cols-3 md:grid-cols-4 gap-4 p-5'>
+    <div className='grid grid-cols-2 sm:grid-cols-3 md:grid-cols-4 gap-4 p-5 bg-gray-600'>
       {data && data.results.map((movie) => {
-        return <div key={movie.id} className='shadow-lg hover:scale-105 ease-in duration-300 cursor-pointer'>
-          <Image
-            src={`https://image.tmdb.org/t/p/w600_and_h900_bestv2${movie.poster_path}`}
-            fallback={<Shimmer width={300} height={400} duration={4} />}
-          />
-
+        return  <div onClick={() => nav(`movie/detail/${movie.id}`, { state: movie })} key={movie.id} className='shadow-lg hover:scale-105 ease-in duration-300 cursor-pointer'>
+          <img src={`https://image.tmdb.org/t/p/w600_and_h900_bestv2${movie.poster_path}`} alt="" />
           <div className='p-4'>
             <h1 className='text-2xl font-bold'>{movie.title ?? movie.name}</h1>
+            <p>{movie.release_date}</p>
             <p>{movie.overview.substring(0, 60) + '....'}</p>
           </div>
         </div>
       })}
     </div>
-
-
-
-
   )
 }
-
+xyx
 export default SearchPage;
